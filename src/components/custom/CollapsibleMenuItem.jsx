509191414740ee105c0a57c1df5f4f53964baefa
macdallas <149372742+MacDallas123@users.noreply.github.com>
2026-01-30 18:47:37 +0100

    Update CollapsibleMenuItem.jsx

--- src/components/custom/CollapsibleMenuItem.jsx
+++ src/components/custom/CollapsibleMenuItem.jsx
@@ -14,7 +14,6 @@ const CollapsibleMenuItem = ({ label, icon, children, closeMobileMenu }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleCloseMobileMenu = (e) => {
-    e.preventDefault();
     closeMobileMenu();
   }
 
