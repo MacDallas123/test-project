a72d70ea797794e6a40fb567b98e83e08b9ab6f3
macdallas <roylexstephane@gmail.com>
2026-01-30 14:52:04 +0100

    Correct header spact on phone

--- src/components/custom/SiteTitleForm1.jsx
+++ src/components/custom/SiteTitleForm1.jsx
@@ -1,7 +1,7 @@
 const SiteTileForm1 = () => {
     return (
-        <div className="flex flex-col px-6 py-1 bg-white rounded-full text-md hover:bg-white/90">
-            <span className="font-semibold text-md md:text-lg bg-linear-to-r from-foreground to-foreground/80 bg-clip-text text-destructive">
+        <div className="flex flex-col px-2 py-1 bg-white rounded-full md:px-6 text-md hover:bg-white/90">
+            <span className="text-xs font-semibold md:text-lg bg-linear-to-r from-foreground to-foreground/80 bg-clip-text text-destructive">
             Livrer Nourriture
             </span>
         </div>
